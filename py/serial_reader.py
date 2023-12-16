1a3161fa4e25df2ab919c536f1a245c6fca9a1d0
Suyash Kumar <suyashkumar2003@gmail.com>
2023-12-16 18:29:21 -0500

    Update serial_reader.py to be resilient to disconnects/reconnects

--- py/serial_reader.py
+++ py/serial_reader.py
@@ -5,17 +5,28 @@ add timestamp data, and write out to disk.
 import serial
 import json
 import datetime
+import time
 
 def main():
-    ser = serial.Serial('/dev/cu.usbmodem14701', 115200)
+    ser = None
     with open('data.ndjson', 'a') as fd:
         while True:
-            line = ser.readline()
+            try:
+                if ser == None:
+                    ser = serial.Serial('/dev/cu.usbmodem14701', 115200)
+                line = ser.readline()
+            except serial.serialutil.SerialException as e:
+                print("serial device not found, retrying in 2s")
+                time.sleep(2)
+                ser = None
+                continue
+
             try:
                 data = json.loads(line)
             except ValueError as e:
                 print(f"Non JSON console data: {line}")
                 continue
+
             data['time'] = datetime.datetime.now().__str__()
             data_with_time = json.dumps(data)
             print(data_with_time)
